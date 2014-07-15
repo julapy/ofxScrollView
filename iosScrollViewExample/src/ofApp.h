0e14dc929b9a89e52f55fb9e5379e8a1e2c9d644
lukasz karluk <julapy@googlemail.com>
2014-07-15 18:06:49 +1000

    prepping projects.

--- iosScrollViewExample/src/ofApp.h
+++ iosScrollViewExample/src/ofApp.h
@@ -6,22 +6,23 @@
 
 class ofApp : public ofxiOSApp{
 	
-    public:
-        void setup();
-        void update();
-        void draw();
-        void exit();
-	
-        void touchDown(ofTouchEventArgs & touch);
-        void touchMoved(ofTouchEventArgs & touch);
-        void touchUp(ofTouchEventArgs & touch);
-        void touchDoubleTap(ofTouchEventArgs & touch);
-        void touchCancelled(ofTouchEventArgs & touch);
+public:
 
-        void lostFocus();
-        void gotFocus();
-        void gotMemoryWarning();
-        void deviceOrientationChanged(int newOrientation);
+    void setup();
+    void update();
+    void draw();
+    void exit();
+	
+    void touchDown(ofTouchEventArgs & touch);
+    void touchMoved(ofTouchEventArgs & touch);
+    void touchUp(ofTouchEventArgs & touch);
+    void touchDoubleTap(ofTouchEventArgs & touch);
+    void touchCancelled(ofTouchEventArgs & touch);
+    
+    void lostFocus();
+    void gotFocus();
+    void gotMemoryWarning();
+    void deviceOrientationChanged(int newOrientation);
 
 };
 
